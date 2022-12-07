8dfc1f61f59863a5a74228574c9df5783372ca6f
Saveliy <88579724+Medvedev-Savelii@users.noreply.github.com>
2022-12-07 08:53:43 +0800

    Update Two_Sum.js

--- Two_Sum.js
+++ Two_Sum.js
@@ -1,1 +1,26 @@
 
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+const sumOfTwo = (arr, target) => {
+  const result = [];
+
+  for (let i = 0; i < arr.length; i++) {
+    
+    for (let j = i + 1; j < arr.length; j++) {
+    
+      if (arr[i] + arr[j] === target) {
+        result.push(i);
+        result.push(j);
+      }
+      
+    }
+    
+  }
+  
+  return result;
+  
+};
+console.log(sumOfTwo([2, 7, 11, 15], 9));
