d685c2489816412ba6ad4d4de3d81cf632ebb6eb
Saveliy <88579724+Medvedev-Savelii@users.noreply.github.com>
2022-12-10 12:26:17 +0800

    Create Contains_Duplicate.js

---
+++ Contains_Duplicate.js
@@ -0,0 +1,16 @@
+/**
+ * @param {number[]} nums
+ * @return {boolean}
+ */
+
+const containsDuplicate = function(nums) {
+  const hash = {};
+  for (let el of nums) {
+    if (hash.hasOwnProperty(el)) {
+      return true;
+    } else {
+      hash[el] = 1;
+    }
+  }
+  return false;
+};
